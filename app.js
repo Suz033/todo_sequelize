b96d5c38e74d3df3fd86876bcfb0a8d7ecb12a35
Suz033 <susanamy2324@gmail.com>
2023-05-19 00:38:38 +0800

    feat: add login and register page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// modules
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
@@ -8,9 +9,33 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+// router
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+// listener
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
