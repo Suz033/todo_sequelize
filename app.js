25c0696ba9830e794bba2859c864ec5b454a1a50
Suz033 <susanamy2324@gmail.com>
2023-05-21 00:03:00 +0800

    feat: add login function

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 
 const PORT = 3000
 
@@ -13,8 +16,14 @@ const User = db.User
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
 // router
 app.get('/', (req, res) => {
@@ -30,9 +39,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
